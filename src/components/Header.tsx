e0421bafeab3d5e72599ad568d12786a863bdb2c
gomo <huberfaceless@gmail.com>
2026-08-19 09:23:07 +0200

    Update Header.tsx
    Header-Logo vollständig anzeigen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -85,7 +85,7 @@ export const Header: React.FC<HeaderProps> = ({
                 <img 
                   src={gommarLogo} 
                   alt="GOM-MAR Academy Logo" 
-                  className="w-full h-full object-cover rounded-xl"
+                  className="w-full h-full object-contain rounded-xl"
                   referrerPolicy="no-referrer"
                 />
               </div>
